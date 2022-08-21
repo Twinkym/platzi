bf2cc927b21e25352d6e52c97a115f31eb87f629
twink <twinkymen@hotmail.com>
2022-08-21 20:32:58 +0200

    inclusion variables

--- JS/mokepon.js
+++ JS/mokepon.js
@@ -1,8 +1,21 @@
-function seleccionarMascotaJugador() {
-  alert("ELEGISTE TU MASCOTA");
+function iniciarJuego() {
+  let botonMascotaJugador = document.getElementById("boton-mascota");
+  botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 }
-let botonMascotaJugador = document.getElementById("boton-mascota");
-botonMascotaJugador = document.addEventListener(
-  "click",
-  seleccionarMascotaJugador
-);
+
+function seleccionarMascotaJugador() {
+  let inputHipoge = document.getElementById("Hipoge");
+  let inputCapipego = document.getElementById("Capipego");
+  let inputRatigueya = document.getElementById("Ratigueya");
+
+  if (inputHipoge.checked) {
+    alert("Elegiste a Hipoge");
+  } else if (inputCapipego.checked) {
+    alert("Elegiste a Capipego");
+  } else if (inputRatigueya.checked) {
+    alert("Elegiste a Ratigueya");
+  } else {
+    alert("Elige a una mascota");
+  }
+}
+window.addEventListener("load", iniciarJuego);
